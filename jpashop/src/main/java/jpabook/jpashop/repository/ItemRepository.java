c79577bce28d1bbc958ea7b2c8dc38f88a8aecf0
profoundsea25 <profoundsea25@gmail.com>
2021-12-06 01:33:13 +0900

    완강 21.12.05

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -17,6 +17,8 @@ public class ItemRepository {
         if (item.getId() == null) {
             em.persist(item);
         } else {
+            // 병합 방법을 사용 (모든 값을 업데이트! 필드값이 없으면 null로 업데이트 하므로 주의!)
+            // 실무에서는 null 값 업데이트가 위험하므로 병합은 지양하는 것이 좋다.
             em.merge(item); // update와 비슷
         }
     }
